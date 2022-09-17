8f2157911bfa523ad2d3a88b75918913c3ca2014
124tranvita <124tranvita@gmail.com>
2022-09-17 22:31:04 +0700

    add vercel.json

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+// const hpp = require('hpp');
 const cors = require('cors');
 
 const userRouter = require('./routes/userRoutes');
@@ -16,13 +20,33 @@ const app = express();
 app.use(cors());
 
 // 1) GLOBAL MIDDLEWARE
+// Set security HTTP headers
+app.use(helmet()); //alway putting in begining
 // Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from same IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000, // milisecond convert for 1 hours
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
 // Body parser
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 // 2) ROUTES
 app.use('/api/v1/users', userRouter);
